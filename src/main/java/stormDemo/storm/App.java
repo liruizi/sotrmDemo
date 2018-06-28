c3a84571bdbfb5b1a2de1d7985064d6cae812649
liruizi <liruizi@liruizi>
2018-06-28 17:39:24 +0800

    ces ces ces

---
+++ src/main/java/stormDemo/storm/App.java
@@ -0,0 +1,13 @@
+package stormDemo.storm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
